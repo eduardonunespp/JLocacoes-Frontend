ab4f544bbbdcdc33a07e0316d021a4b237e86ec4
eduardonunespp <carlospn200406@gmail.com>
2023-09-25 11:49:34 -0300

    refactor: app

--- JLocacoes/src/main.tsx
+++ JLocacoes/src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ThemeProvider } from "styled-components";
+import App from "./App.tsx";
+import GlobalStyles from './styles/global'
+import theme from "./styles/theme";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <ThemeProvider theme={theme}>
+    <GlobalStyles />
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+    ,
+  </ThemeProvider>
+);
