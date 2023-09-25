00bd0a61f237b4fb6ffaa45a373e0db95bf90726
Carlos Eduardo <carlospn200406@gmail.com>
2023-09-25 16:05:08 -0300

    feat: add sweet-alert

---
+++ JLocacoes/src/shared/core/sweet-alert/sweet-alert.ts
@@ -0,0 +1,35 @@
+import Swal from "sweetalert2";
+
+import { CallAlert, CallSuccessAlert, CallErrorAlert } from "../domain-types";
+
+const styledSwal = Swal.mixin({
+  buttonsStyling: true,
+  confirmButtonColor: "#fff",
+  showConfirmButton: false,
+});
+
+export const call: CallAlert = ({ type, title, description, onConfirm }) => {
+  void styledSwal
+    .fire({
+      icon: type,
+      title,
+      text: description,
+      allowOutsideClick: false,
+      returnFocus: false,
+    })
+    .then((isConfirm) => {
+      if (isConfirm) {
+        if (onConfirm) {
+          onConfirm();
+        }
+      }
+    });
+};
+
+export const callSuccess: CallSuccessAlert = (input) => {
+  return call({ type: "success", ...input });
+};
+
+export const callError: CallErrorAlert = (input) => {
+  return call({ type: "error", ...input });
+};
