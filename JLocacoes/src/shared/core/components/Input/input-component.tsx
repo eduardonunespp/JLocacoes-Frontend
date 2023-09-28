578fe78bca5a5ccd27587409a38ee2bb4aca6a7a
eduardonunespp <carlospn200406@gmail.com>
2023-09-28 08:26:12 -0300

    feat: add Input component

---
+++ JLocacoes/src/shared/core/components/Input/input-component.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import * as S from "./input-component.styles";
+import { IInput } from "../../domain-types";
+import { forwardRef } from "react";
+
+export const InputAuth = forwardRef<HTMLInputElement, IInput>(
+  ({ label, id, placeholder, error, ...rest }, ref) => {
+    const getErrorStyles = (error: string | undefined) => {
+      return error !== undefined;
+    };
+
+    return (
+      <S.InputAuth error={getErrorStyles(error)}>
+        <label htmlFor={id}>{label}</label>
+        <input {...rest} ref={ref} placeholder={placeholder} />
+        {error !== undefined && <span>{error}</span>}
+      </S.InputAuth>
+    );
+  }
+);
+
+export default InputAuth;
